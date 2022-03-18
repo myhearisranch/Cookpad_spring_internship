fe7814704ea3ce27b345a7f3b9ef85c992d1fb36
myhearisranch <myhearisranch@gmail.com>
2022-03-18 16:28:00 +0900

    クックパッドスプリングインターン

---
+++ introduce.rb
@@ -0,0 +1,7 @@
+#puts "\e[31mHello\e[m" + "\e[34mWorld!\em"
+
+ s = %q[puts "s = %q["s"]; eval s"];eval s
+
+ss
+
+ #s = %q[puts "s = %q["+s+"]; eval s]; eval s
---
+++ work5.rb
@@ -0,0 +1,16 @@
+require_relative "./work4.rb"
+
+
+
+eval(%w(
+
+
+  p                u
+    t       s ( $ a bcdefg
+     h         i      j
+               k      l
+  m            n      o
+    p          q      r
+
+    s       t u v w x y z )
+)*"")
